d4aefd1b39f6bc415da736f8adf529778e64ae52
DiMoon <devilnoisy999@gmail.com>
2023-08-03 21:46:33 +0500

    Autofill 1

--- DataBaze/Source/Private/Components/DBAutofill.cpp
+++ DataBaze/Source/Private/Components/DBAutofill.cpp
@@ -1,5 +1,23 @@
 #include "DBAutofill.h"
 
-void DBAutofill::Tick(float DeltaTime)
+void DBAutofill::Init(const DBFamilyData& InUserData)
 {
 }
+
+void DBAutofill::StartFilling()
+{
+}
+
+void DBAutofill::Tick(float DeltaTime)
+{
+	if (ActionList.size() == 0 || CurrentActionIndex >= ActionList.size()) return;
+
+	auto Action = ActionList[CurrentActionIndex];
+	Action->DoAction();
+	Wait(Action->DelaySeconds);
+	++CurrentActionIndex;
+}
+
+void DBAction_PressButtons::DoAction() {}
+
+void DBAction_Clipboard::DoAction() {}
--- DataBaze/Source/Public/Components/DBAutofill.h
+++ DataBaze/Source/Public/Components/DBAutofill.h
@@ -1,9 +1,48 @@
 #pragma once
 #include "DBInterface.h"
 #include "SimpleThread/Public/STClient.h"
+#include "DBDataTypes.h"
+#include <vector>
+
+class DBAction;
 
 class DBAutofill : public DBInterface, public STClient
 {
 private:
+	std::vector<DBAction*> ActionList;
+	int CurrentActionIndex = 0;
+
+public:
+	void Init(const DBFamilyData& InUserData);
+	void StartFilling();
+
+private:
 	virtual void Tick(float DeltaTime) override;
+};
+
+class DBAction
+{
+public:
+	virtual ~DBAction() {}
+	float DelaySeconds = 0.f; // delay at the end
+public:
+	virtual void DoAction() = 0;
+};
+
+/*
+/* Press hotkey or type a text
+*/
+class DBAction_PressButtons : public DBAction
+{
+public:
+	virtual void DoAction() override;
+};
+
+/*
+/* Copy to or Paste Clipboard
+*/
+class DBAction_Clipboard : public DBAction
+{
+public:
+	virtual void DoAction() override;
 };
